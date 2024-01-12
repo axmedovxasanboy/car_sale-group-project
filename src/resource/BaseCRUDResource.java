e22d50e6419d86f6e213c4145a8295e53b7b0810
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-01-12 19:56:55 +0500

    first commit

---
+++ src/resource/BaseCRUDResource.java
@@ -0,0 +1,16 @@
+package resource;
+
+import bean.ApiResponse;
+import bean.BaseBean;
+
+public interface BaseCRUDResource<T extends BaseBean> {
+    ApiResponse add(T bean);
+
+    ApiResponse get(Integer id);
+
+    ApiResponse edit(T bean);
+
+    ApiResponse delete(Integer id);
+
+
+}
