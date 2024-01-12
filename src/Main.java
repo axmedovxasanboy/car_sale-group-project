dfe202e6b737ad1542537462d72676bd82080cf3
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-01-12 20:18:39 +0500

    added 1 todo in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Online Car sale project");
+//        TODO: Kim qayerini qilishini ertaga shanba kuni kelishamiz, nasib bo'lsa
     }
 }
