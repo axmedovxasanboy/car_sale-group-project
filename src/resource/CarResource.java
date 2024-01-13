3699a1e31b20a28df3d8fabede7297e1df920b61
jack-sparrow-18-04 <akhmedovkhasanboy28@gmail.com>
2024-01-13 23:16:21 +0500

    Added to do tasks

--- src/resource/CarResource.java
+++ src/resource/CarResource.java
@@ -5,6 +5,7 @@ public class CarResource {
     TODO: For Azizbek Qudratov
     TODO: Make this class implementation of BaseCRUDResource class using Generics
      Generate all methods from BASECRUDResource using suitable types
+     For example: Add method should take 1 argument which is in CarBean type
      All methods should return "null" for now.
 */
 }
--- src/resource/UserResource.java
+++ src/resource/UserResource.java
@@ -5,6 +5,7 @@ public class UserResource {
     TODO for Azizbek Qudratov
     TODO: Make this class implementation of BaseCRUDResource class using Generics
      Generate all methods from BASECRUDResource using suitable types
+     For example: Add method should take 1 argument which is in UserBean type
      All methods should return "null" for now.
     */
 }
