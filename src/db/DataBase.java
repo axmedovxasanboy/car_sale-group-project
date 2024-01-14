7d4446273ff24d17b2482c5b49ba2874dfbeca98
user <abdulazizovfayzu;llo0@gmail.com>
2024-01-15 00:06:50 +0500

    Xasanboy na gap

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -1,6 +1,29 @@
 package db;
 
+import bean.CarBean;
+import bean.UserBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBase {
-  // TODO for Fayzullo
-  // TODO: create 2 List Arrays for users and cars. They should get only UserBean and Carbean respectively.
+
+    protected List<UserBean> users = new ArrayList<UserBean>();
+    protected List<CarBean> cars = new ArrayList<CarBean>();
+
+    public List<UserBean> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserBean> users) {
+        this.users = users;
+    }
+
+    public List<CarBean> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<CarBean> cars) {
+        this.cars = cars;
+    }
 }
