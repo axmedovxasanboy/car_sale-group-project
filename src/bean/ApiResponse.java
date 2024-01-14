365232eacdf38f42e1fcbe883075f5bf1e6fae9e
Jack Sparrow <113936047+axmedovxasanboy@users.noreply.github.com>
2024-01-14 08:50:49 +0500

    Todo tasks added

--- src/bean/ApiResponse.java
+++ src/bean/ApiResponse.java
@@ -1,4 +1,11 @@
 package bean;
 
 public class ApiResponse {
+  
+/*
+    TODO: For Shahnoza
+    TODO: Create variables: Integer code, String message, Object data
+     Create getter and setters
+     Generate 2 constructors 1. no args constructor. 2. all args constructor
+*/
 }
