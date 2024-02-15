c9855df9144765539c2b7424af2d94c0083e6680
Fayzullo <92589236+CoFayzi@users.noreply.github.com>
2024-02-15 22:15:59 +0500

    Fayzullo is done

--- src/resource/CarResource.java
+++ src/resource/CarResource.java
@@ -41,30 +41,22 @@ public class CarResource implements BaseCRUDResource<CarBean>{
     }
 
     public ApiResponse getCarsInStore(Integer userId) {
-        /*
-         * TODO Fayzullo
-         *  Write a logic in carsInStore method which is in Database
-         *  Get a variable to that list using carsInStore method
-         *  and return new ApiResponse if list is empty "Empty list" with 400 code
-         *  and if not return "Car List" with 200 code and list of that cars
-         * */
-        return null;
+        List<CarBean> cars = DataBase.carsInStore(userId);
+        return cars.isEmpty() ? new ApiResponse(400, "Empty List!", null) :
+                new ApiResponse(200, "Car List", cars);
     }
 
     public ApiResponse getCarsNotInStore(Integer userId) {
-//        TODO it returns the same as getCarsInStore method (copy paste) (Fayzullo)
-        return null;
+        List<CarBean> cars = DataBase.carsNotInStore(userId);
+        return cars.isEmpty() ? new ApiResponse(400, "Empty List!", null) :
+                new ApiResponse(200, "Car List", cars);
+
     }
 
     public ApiResponse takeCar(int carId, Integer userId) {
-        /*
-        * TODO Fayzullo
-        *  take result of that takeCar method to new variable
-        *  and if variable is true return new ApiResponse with 200 code, "Success" message and true as an object
-        *  if false code 400, "Error Occurred" message with false object
-        *  <<Note takeCar method is written in DataBase class>>
-        *  */
-        return null;
+        Boolean takeCar = DataBase.takeCar(carId, userId);
+        return takeCar ? new ApiResponse(200, "Success", true) :
+                new ApiResponse(400, "Error occurred", false);
     }
 
 }
